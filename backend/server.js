731b8f919a69c812660e0f259ee503275bffb5dd
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-05 18:31:32 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 require('dotenv').config();
+const bot = require('./bot');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
