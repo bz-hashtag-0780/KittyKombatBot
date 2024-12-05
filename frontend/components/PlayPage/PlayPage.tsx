c1536da0df2e948c13a3351ceb812a0facd5e20b
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-05 16:15:46 +0100

    Update PlayPage.tsx

--- frontend/components/PlayPage/PlayPage.tsx
+++ frontend/components/PlayPage/PlayPage.tsx
@@ -25,7 +25,7 @@ export const PlayPage = () => {
 					<div>
 						<div className="flex items-center gap-1">
 							<span className="font-semibold text-white">
-								{initData}
+								{/* {initData} */}@blubbernugget
 							</span>
 							<BadgeCheck className="w-4 h-4 text-blue-500" />
 						</div>
